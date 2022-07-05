c9d4c505794a623ab0ad05747b2a4f26d249a12d
Brice Vandeputte <brice.vandeputte@orange.com>
2022-07-05 20:33:59 +0200

    Fix #5 improve umami report
    provide results as output, env, file
    upd readme, add npm audit

--- main.js
+++ main.js
@@ -6,23 +6,32 @@ const umamiServer = core.getInput('umami-server', { required: true });
 var umamiUser = core.getInput('umami-user', { required: true });
 const umamiPassword = core.getInput('umami-password', { required: true });
 const umamiSiteDomain = core.getInput('umami-site-domain');// *first*
+const umamiReportFile = core.getInput('umami-report-file');// ''
 const rethrow = (err) => {throw err;};
+
 const printContext = () => {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
 };
-try {
-  if (umamiServer === null || umamiServer === undefined) {
-    throw "please setup your environment"
-  }
-  if (umamiUser === null || umamiUser === undefined) {
-    umamiUser = 'admin';
-  }
-  // printContext();
-  const reportResult = await action.umamiDailyReportV0(umamiServer, umamiUser, umamiPassword, umamiSiteDomain).catch(rethrow);
-  core.info(`Generated : ${reportResult.targetFile}`);
-} catch (error) {
-  console.info(`ERROR: ${error}`)
-  core.setFailed(error);
+
+const actionUmamiReport = async function() {
+    try {
+      if (umamiServer === null || umamiServer === undefined) {
+        throw "please setup your environment"
+      }
+      if (umamiUser === null || umamiUser === undefined) {
+        umamiUser = 'admin';
+      }
+      // printContext();
+      const reportResult = await action.umamiDailyReportV0(umamiServer, umamiUser, umamiPassword, umamiSiteDomain, umamiReportFile).catch(rethrow);
+      if (reportResult.targetFile !== null) {
+        core.info(`Generated : ${reportResult.targetFile}`);
+      }
+    } catch (error) {
+      console.info(`ERROR: ${error}`)
+      core.setFailed(error);
+    }
 }
+
+actionUmamiReport();
--- manual.js
+++ manual.js
@@ -6,15 +6,22 @@ const UMAMI_SERVER = process.env.UMAMI_SERVER || null; // "https://umami.exemple
 const UMAMI_USER = process.env.UMAMI_USER || "admin";
 const UMAMI_PASSWORD = process.env.UMAMI_PASSWORD || null;
 const UMAMI_SITE_DOMAIN = process.env.UMAMI_SITE_DOMAIN || "*first*";
+const UMAMI_REPORT_FILE = process.env.UMAMI_REPORT_FILE || "umamiReport.txt";
 const rethrow = (err) => {throw err;}
 
-try {
-  if (UMAMI_SERVER === null) {
-    throw "please setup your environment UMAMI_SERVER, UMAMI_USER, UMAMI_PASSWORD, UMAMI_SITE_DOMAIN"
-  }
-  const reportResult = await action.umamiDailyReportV0(UMAMI_SERVER, UMAMI_USER, UMAMI_PASSWORD, UMAMI_SITE_DOMAIN)
-                                   .catch(rethrow);
-  console.info(`Generated : ${reportResult.targetFile}`);
-} catch (error) {
-  console.info(`ERROR: ${error}`)
+const actionUmamiReport = async function() {
+    try {
+      if (UMAMI_SERVER === null) {
+        throw "please setup your environment UMAMI_SERVER, UMAMI_USER, UMAMI_PASSWORD, UMAMI_SITE_DOMAIN"
+      }
+      const reportResult = await action.umamiDailyReportV0(UMAMI_SERVER, UMAMI_USER, UMAMI_PASSWORD, UMAMI_SITE_DOMAIN, UMAMI_REPORT_FILE)
+                                       .catch(rethrow);
+      if ('targetFile' in reportResult) {
+        console.info(`Generated : ${reportResult.targetFile}`);
+      }
+    } catch (error) {
+      console.info(`ERROR: ${error}`)
+    }
 }
+
+actionUmamiReport();
