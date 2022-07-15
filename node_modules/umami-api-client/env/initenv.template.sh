f3c7ace8eef0f45a5b79c75cce1e0cd8369e71c1
Jakob Bouchard <jakob@jakobbouchard.dev>
2022-07-15 10:33:03 -0400

    Change to use umami-api, remove node-fetch

--- node_modules/umami-api-client/env/initenv.template.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export UMAMI_SERVER="https://umami.replace-me.exemple.com"
-export UMAMI_USER="admin"
-export UMAMI_PASSWORD="12333321"
-# export UMAMI_SITE_DOMAIN="replace-me.exemple.com"
-export UMAMI_SITE_DOMAIN="*first*"
-
-### Dev debug tests
-# export TEST_VERBOSE=true
