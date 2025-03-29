f034623224dd7e0506141c8628739366ac3f1c19
Brice Vandeputte <brice.vandeputte@orange.com>
2025-03-29 15:19:26 +0100

    pnpm qa6 - main.js => index/js everywhere

--- .github/scripts/package.sh
+++ .github/scripts/package.sh
@@ -2,6 +2,9 @@
 # GithubActions - skip node_modules in git : cf. https://github.com/orgs/community/discussions/102976
 # use @vercel/ncc to package action        : cf. https://docs.github.com/en/actions/sharing-automations/creating-actions/creating-a-javascript-action#commit-tag-and-push-your-action-to-github
 
+# exit on first error
+set -e
+
 # check `ncc` requirement
 if ! command -v ncc &> /dev/null
 then
