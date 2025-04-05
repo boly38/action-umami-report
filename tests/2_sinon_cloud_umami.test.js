f4d5d8cb1ed268eb11722553b841a55cbed530db
Brice Vandeputte <brice.vandeputte@orange.com>
2025-04-05 20:47:28 +0200

    Fix #80 support cloud mode
    CR Pilot

---
+++ tests/2_sinon_cloud_umami.test.js
@@ -0,0 +1,34 @@
+import {env} from 'node:process';
+import {afterEach, beforeEach, describe, it} from "mocha";
+import core from '@actions/core';
+import sinon from 'sinon';
+
+import UmamiReport from "../lib/umamiReport.js";
+import {verboseStringify} from "./testUtil.js";
+
+describe("Test action-umami-report sinon based cloud githubActionReport", function () {
+    let getInputStub;
+    let getBooleanInputStub;
+
+    beforeEach(() => {
+        // Mock de `core.getInput`
+        getInputStub = sinon.stub(core, 'getInput');
+        getBooleanInputStub = sinon.stub(core, 'getBooleanInput');
+    });
+
+    afterEach(() => {
+        getInputStub.restore();
+        getBooleanInputStub.restore();
+    });
+
+    it("should report from umami cloud with default options (1day)", async function () {
+        getInputStub.withArgs('umami-cloud-api-key').returns(env["UMAMI_TEST_CLOUD_API_KEY"]);
+        getInputStub.withArgs('umami-site-domain').returns(env["UMAMI_TEST_CLOUD_SITE_DOMAIN"]);
+        getBooleanInputStub.withArgs('umami-prefetch', false).returns(false);
+
+        await UmamiReport.githubActionReport()
+            .then(verboseStringify).catch(console.error);
+    });
+
+
+});
