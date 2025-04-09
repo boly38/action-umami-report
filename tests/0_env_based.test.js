8b2f4b660360805aa5253c50bcd85f682f5eeb59
Brice Vandeputte <brice.vandeputte@orange.com>
2025-04-09 08:29:47 +0200

    no view no report, minor report format upd, fix pageViews abscissa

--- tests/0_env_based.test.js
+++ tests/0_env_based.test.js
@@ -1,5 +1,5 @@
 import UmamiReport from "../lib/umamiReport.js";
-import {verboseStringify} from "./testUtil.js";
+import {expectNoError, verboseStringify} from "./testUtil.js";
 import {describe, before, it} from "mocha";
 
 let options;
@@ -17,35 +17,35 @@ describe("Env based tests", function () {
 
     it("should report using default options (1day)", async function () {
         await UmamiReport.manualReport(UmamiReport.getOptions())
-            .then(verboseStringify).catch(console.error);
+            .then(verboseStringify).catch(expectNoError);
     });
 
     it("should report 1week (u:day)", async function () {
         await UmamiReport.manualReport(UmamiReport.getOptions({
             "period": "1week",
             "unit": "day"
-        })).then(verboseStringify).catch(console.error);
+        })).then(verboseStringify).catch(expectNoError);
     });
 
     it("should report 1week (u:hour)", async function () {
         await UmamiReport.manualReport(UmamiReport.getOptions({
             "period": "1week",
             "unit": "hour"
-        })).then(verboseStringify).catch(console.error);
+        })).then(verboseStringify).catch(expectNoError);
     });
 
     it("should report 1month (u:day)", async function () {
         await UmamiReport.manualReport(UmamiReport.getOptions({
             "period": "1month",
             "unit": "day"
-        })).then(verboseStringify).catch(console.error);
+        })).then(verboseStringify).catch(expectNoError);
     });
 
     it("should report 1month (u:hour)", async function () {
         await UmamiReport.manualReport(UmamiReport.getOptions({
             "period": "1month",
             "unit": "hour"
-        })).then(verboseStringify).catch(console.error);
+        })).then(verboseStringify).catch(expectNoError);
     });
 
 });
